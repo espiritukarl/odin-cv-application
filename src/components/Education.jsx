f217c4d57af2e1ba02c3ed268b975744822b10fb
Karl Vincent M. Espiritu <karlespiritukarl@gmail.com>
2024-08-16 04:42:19 +0800

    Made use of useHover hook. Also of InputField, and NewButton. Also refactored for better readability of code.

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import placeholder from "../data/placeholder.json";
 import "../styles/form.css";
-import NewButton from "./FormBtn";
-
 import Icon from "@mdi/react";
+import useHover from "../hooks/useHover";
+import InputField from "./InputField";
 import {
   mdiDelete,
   mdiDeleteEmpty,
@@ -12,43 +12,66 @@ import {
   mdiWindowClose,
   mdiPlusBox,
 } from "@mdi/js";
+import NewButton from "./NewButton";
 
 function Education({ updateCV, educationData, school, index }) {
   const [info, setInfo] = useState(school);
-  const [isHovered, setIsHovered] = useState(false);
+  const [isHovered, handleMouseEnter, handleMouseLeave] = useHover();
   const [showEducationContent, setShowEducationContent] = useState(false);
   const showAddEducation = educationData.length > index;
+  const noEntry =
+    JSON.stringify(info.education) === JSON.stringify(placeholder.education);
+
+  const showForm = () => {
+    if (showEducationContent) {
+      return (
+        <span className="close-form">
+          <button
+            onClick={() => setShowEducationContent(!showEducationContent)}
+          >
+            <Icon path={mdiWindowClose} size={0.8} />
+          </button>
+        </span>
+      );
+    } else {
+      return (
+        <span className="new-form">
+          <button
+            className="poppins-regular"
+            onClick={() => setShowEducationContent(!showEducationContent)}
+          >
+            <span>Add New School</span>
+            <Icon path={mdiPlusBox} size={0.85} />
+          </button>
+        </span>
+      );
+    }
+  };
 
   function handleChange(e) {
     const { name, value } = e.target;
-    setInfo((prevInfo) => ({
-      ...prevInfo,
-      [name]: value,
-    }));
+    setInfo((prevInfo) => ({ ...prevInfo, [name]: value }));
   }
 
-  function editEntry() {
-    let updatedEducation = [...educationData];
-    updatedEducation[index] = info;
+  function handleEntry(action) {
+    const updatedEducation = [...educationData];
+    if (action === "edit") {
+      updatedEducation[index] = info;
+    } else if (action === "remove") {
+      updatedEducation.splice(index, 1);
+    }
     updateCV("education", updatedEducation);
+    setShowEducationContent(false);
   }
 
-  function removeEntry() {
-    let updatedEducation = [...educationData];
-    updatedEducation.splice(index, 1);
-    updateCV("education", updatedEducation);
-  }
-
-  function handleSubmit() {
-    const updatedEducation = [...educationData, info];
-    updateCV("education", updatedEducation);
-
-    handleEducationContent();
-    setInfo(placeholder.education);
-  }
-
-  function handleEducationContent() {
-    setShowEducationContent(!showEducationContent);
+  function handleSubmit(action) {
+    if (action === "add") {
+      updateCV("education", [...educationData, info]);
+    } else if (action === "empty") {
+      updateCV("education", [info]);
+    }
+    setInfo(placeholder.education[0]);
+    setShowEducationContent(false);
   }
 
   const isFormValid = ["school", "title", "startdate", "enddate", "loc"].every(
@@ -57,113 +80,80 @@ function Education({ updateCV, educationData, school, index }) {
 
   return (
     <>
-      {showAddEducation ? (
-        <div className="list-header">
-          <span className="poppins-regular">{school.school}</span>
-          <span className="list-button-container">
-            <Icon
-              path={
-                showEducationContent ? mdiWindowMinimize : mdiWindowMaximize
-              }
-              size={0.8}
-              onClick={handleEducationContent}
-              className="section-expand"
-            />
-            <Icon
-              onMouseEnter={() => setIsHovered(true)}
-              onMouseLeave={() => setIsHovered(false)}
-              onClick={removeEntry}
-              path={isHovered ? mdiDeleteEmpty : mdiDelete}
-              size={0.85}
-              className="section-remove"
-            />
-          </span>
-        </div>
-      ) : !showEducationContent ? (
-        <>
-          <span className="new-form">
-            <button
-              className="poppins-regular"
-              onClick={handleEducationContent}
-            >
-              <span>Add New School</span>
-              <Icon path={mdiPlusBox} size={0.85} />
-            </button>
-          </span>
-        </>
-      ) : (
-        <>
-          <span className="close-form">
-            <button onClick={handleEducationContent}>
-              <Icon path={mdiWindowClose} size={0.8} />
-            </button>
-          </span>
-        </>
-      )}
+      {showAddEducation
+        ? school.school && (
+            <div className="list-header">
+              <span className="poppins-regular">{school.school}</span>
+              <span className="list-button-container">
+                <Icon
+                  path={
+                    showEducationContent ? mdiWindowMinimize : mdiWindowMaximize
+                  }
+                  size={0.8}
+                  onClick={() => setShowEducationContent(!showEducationContent)}
+                  className="section-expand"
+                />
+                <Icon
+                  onMouseEnter={handleMouseEnter}
+                  onMouseLeave={handleMouseLeave}
+                  onClick={() => handleEntry("remove")}
+                  path={isHovered ? mdiDeleteEmpty : mdiDelete}
+                  size={0.85}
+                  className="section-remove"
+                />
+              </span>
+            </div>
+          )
+        : showForm()}
 
       {showEducationContent && (
         <section className="education poppins-regular">
-          <label htmlFor="school">
-            Institution Name
-            <input
-              type="text"
-              name="school"
-              value={info.school}
-              placeholder="e.g. University of the Philippines"
-              onChange={handleChange}
-            />
-          </label>
-          <label htmlFor="title">
-            Program of Study
-            <input
-              type="text"
-              name="title"
-              value={info.title}
-              placeholder="e.g. Bachelor of Science in Computer Science"
-              onChange={handleChange}
-            />
-          </label>
-          <label htmlFor="startdate">
-            Start Date
-            <input
-              type="text"
-              name="startdate"
-              placeholder="e.g March 2018"
-              value={info.startdate}
-              onChange={handleChange}
-            />
-          </label>
-          <label htmlFor="enddate">
-            End Date
-            <input
-              type="text"
-              name="enddate"
-              placeholder="e.g. March 2024"
-              value={info.enddate}
-              onChange={handleChange}
-            />
-          </label>
-          <label htmlFor="loc">
-            Location
-            <input
-              type="text"
-              name="loc"
-              placeholder="e.g. Katipunan, QC"
-              value={info.loc}
-              onChange={handleChange}
-            />
-          </label>
+          <InputField
+            name="school"
+            value={info.school}
+            placeholder="e.g. University of the Philippines"
+            onChange={handleChange}
+            label="Institution Name"
+          />
+          <InputField
+            name="title"
+            value={info.title}
+            placeholder="e.g. Bachelor of Science in Computer Science"
+            onChange={handleChange}
+            label="Program of Study"
+          />
+          <InputField
+            name="startdate"
+            value={info.startdate}
+            placeholder="e.g March 2018"
+            onChange={handleChange}
+            label="Start Date"
+          />
+          <InputField
+            name="enddate"
+            value={info.enddate}
+            placeholder="e.g. March 2024"
+            onChange={handleChange}
+            label="End Date"
+          />
+          <InputField
+            name="loc"
+            value={info.loc}
+            placeholder="e.g. Katipunan, QC"
+            onChange={handleChange}
+            label="Location"
+          />
 
           {!showAddEducation ? (
             <NewButton
-              onClick={handleSubmit}
+              onClick={() => handleSubmit(noEntry ? "empty" : "add")}
               isFormValid={isFormValid}
               className="section-submit"
               buttonText="Add Education"
             />
           ) : (
             <NewButton
-              onClick={editEntry}
+              onClick={() => handleEntry("edit")}
               isFormValid={isFormValid}
               className="section-edit"
               buttonText="Edit"
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -11,27 +11,52 @@ import {
   mdiPlusBox,
   mdiPlusCircle,
 } from "@mdi/js";
-import NewButton from "./FormBtn";
+import NewButton from "./NewButton";
 import TextArea from "./TextArea";
+import InputField from "./InputField";
+import useHover from "../hooks/useHover";
 
 function Work({ updateCV, workData, exp, index }) {
   const [info, setInfo] = useState(exp);
-  const [isHovered, setIsHovered] = useState(false);
   const [showWorkContent, setShowWorkContent] = useState(false);
   const [newResponsibility, setNewResponsibility] = useState("");
   const showAddWork = workData.length > index;
+  const noEntry =
+    JSON.stringify(info.experience) ===
+    JSON.stringify(placeholder.experience[0]);
+  const [isHovered, handleMouseEnter, handleMouseLeave] = useHover();
+
+  const showForm = () => {
+    if (showWorkContent) {
+      return (
+        <span className="close-form">
+          <button onClick={() => setShowWorkContent(!showWorkContent)}>
+            <Icon path={mdiWindowClose} size={0.8} />
+          </button>
+        </span>
+      );
+    } else {
+      return (
+        <span className="new-form">
+          <button
+            className="poppins-regular"
+            onClick={() => setShowWorkContent(!showWorkContent)}
+          >
+            <span>Add New Company</span>
+            <Icon path={mdiPlusBox} size={0.85} />
+          </button>
+        </span>
+      );
+    }
+  };
 
   function handleChange(e) {
     const { name, value } = e.target;
-    setInfo((prevInfo) => ({
-      ...prevInfo,
-      [name]: value,
-    }));
+    setInfo((prevInfo) => ({ ...prevInfo, [name]: value }));
   }
 
   function handleDescription(e) {
-    const { value } = e.target;
-    setNewResponsibility(value);
+    setNewResponsibility(e.target.value);
   }
 
   function addResponsibility() {
@@ -39,64 +64,41 @@ function Work({ updateCV, workData, exp, index }) {
       ...info.responsibilities,
       newResponsibility,
     ];
-
-    setInfo((prevInfo) => ({
-      ...prevInfo,
-      responsibilities: updatedResponsibilities,
-    }));
-    responsibilityChanges(workData, updatedResponsibilities);
+    updateWorkData({ ...info, responsibilities: updatedResponsibilities });
     setNewResponsibility("");
   }
 
   function updateResponsibility(data) {
-    const updatedResponsibilities = [...data];
-
-    setInfo((prevInfo) => ({
-      ...prevInfo,
-      responsibilities: updatedResponsibilities,
-    }));
-
-    let updatedExperience = [...workData];
-    updatedExperience[index] = {
-      ...info,
-      responsibilities: updatedResponsibilities,
-    };
-    responsibilityChanges(workData, updatedResponsibilities);
+    updateWorkData({ ...info, responsibilities: data });
   }
 
   function removeResponsibility(idx) {
-    let updatedExperience = [...workData];
+    const updatedExperience = [...workData];
     updatedExperience[index].responsibilities.splice(idx, 1);
     updateCV("experience", updatedExperience);
   }
 
-  function responsibilityChanges(data, updatedData) {
-    let updatedExperience = [...data];
-    updatedExperience[index] = {
-      ...info,
-      responsibilities: updatedData,
-    };
-
+  function updateWorkData(updatedInfo) {
+    const updatedExperience = [...workData];
+    updatedExperience[index] = updatedInfo;
     updateCV("experience", updatedExperience);
-  }
-
-  function editEntry() {
-    let updatedExperience = [...workData];
-    updatedExperience[index] = info;
-    updateCV("experience", updatedExperience);
+    setInfo(updatedInfo);
   }
 
   function removeEntry() {
-    let updatedExperience = [...workData];
+    const updatedExperience = [...workData];
     updatedExperience.splice(index, 1);
     updateCV("experience", updatedExperience);
   }
 
-  function handleSubmit() {
-    const updatedExperience = [...workData, info];
-    updateCV("experience", updatedExperience);
-
-    setInfo(placeholder.experience);
+  function handleSubmit(action) {
+    if (action === "add") {
+      updateCV("experience", [...workData, info]);
+    } else if (action === "empty") {
+      updateCV("experience", [info]);
+    }
+    setInfo(placeholder.experience[0]);
+    setShowWorkContent(false);
   }
 
   function handleWorkContent() {
@@ -113,70 +115,48 @@ function Work({ updateCV, workData, exp, index }) {
 
   return (
     <>
-      {showAddWork ? (
-        <div className="list-header">
-          <span className="poppins-regular">{exp.company}</span>
-          <span className="list-button-container">
-            <Icon
-              path={showWorkContent ? mdiWindowMinimize : mdiWindowMaximize}
-              size={0.8}
-              onClick={handleWorkContent}
-              className="section-expand"
-            />
-            <Icon
-              onMouseEnter={() => setIsHovered(true)}
-              onMouseLeave={() => setIsHovered(false)}
-              onClick={removeEntry}
-              path={isHovered ? mdiDeleteEmpty : mdiDelete}
-              size={0.85}
-              className="section-remove"
-            />
-          </span>
-        </div>
-      ) : !showWorkContent ? (
-        <>
-          <span className="new-form">
-            <button className="poppins-regular" onClick={handleWorkContent}>
-              <span>Add New Company</span>
-              <Icon path={mdiPlusBox} size={0.85} />
-            </button>
-          </span>
-        </>
-      ) : (
-        <>
-          <span className="close-form">
-            <button onClick={handleWorkContent}>
-              <Icon path={mdiWindowClose} size={0.8} />
-            </button>
-          </span>
-        </>
-      )}
+      {showAddWork
+        ? exp.company && (
+            <div className="list-header">
+              <span className="poppins-regular">{exp.company}</span>
+              <span className="list-button-container">
+                <Icon
+                  path={showWorkContent ? mdiWindowMinimize : mdiWindowMaximize}
+                  size={0.8}
+                  onClick={handleWorkContent}
+                  className="section-expand"
+                />
+                <Icon
+                  onMouseEnter={handleMouseEnter}
+                  onMouseLeave={handleMouseLeave}
+                  onClick={removeEntry}
+                  path={isHovered ? mdiDeleteEmpty : mdiDelete}
+                  size={0.85}
+                  className="section-remove"
+                />
+              </span>
+            </div>
+          )
+        : showForm()}
 
       {showWorkContent && (
         <section className="work-experience poppins-regular">
-          <label htmlFor="company">
-            Company Name
-            <input
-              type="text"
-              name="company"
-              value={info.company}
-              placeholder="e.g. Netflix Inc."
-              onChange={handleChange}
-            />
-          </label>
+          <InputField
+            label="Company Name"
+            name="company"
+            value={info.company}
+            placeholder="e.g. Netflix Inc."
+            onChange={handleChange}
+          />
+          <InputField
+            label="Job Title"
+            name="position"
+            value={info.position}
+            placeholder="e.g. Junior Web Developer"
+            onChange={handleChange}
+          />
 
-          <label htmlFor="position">
-            Job Title
-            <input
-              type="text"
-              name="position"
-              value={info.position}
-              placeholder="e.g. Junior Web Developer"
-              onChange={handleChange}
-            />
-          </label>
-
-          <label htmlFor="responsibilities">Job Responsibilities</label>
+          <span>Job Responsibilities</span>
           {exp.responsibilities &&
             exp.responsibilities.map((resp, i) => (
               <div className="responsibilities" key={`${resp}${i}`}>
@@ -189,7 +169,6 @@ function Work({ updateCV, workData, exp, index }) {
                 />
               </div>
             ))}
-
           <textarea
             type="text"
             name="responsibilities"
@@ -201,49 +180,38 @@ function Work({ updateCV, workData, exp, index }) {
             <Icon path={mdiPlusCircle} size={1} />
           </button>
 
-          <label htmlFor="startdate">
-            Start Date
-            <input
-              type="text"
-              name="startdate"
-              value={info.startdate}
-              placeholder="e.g. September 2021"
-              onChange={handleChange}
-            />
-          </label>
-
-          <label htmlFor="enddate">
-            End Date
-            <input
-              type="text"
-              name="enddate"
-              value={info.enddate}
-              placeholder="e.g. March 2024"
-              onChange={handleChange}
-            />
-          </label>
-
-          <label htmlFor="loc">
-            Location
-            <input
-              type="text"
-              name="loc"
-              placeholder="e.g. New Delhi, India"
-              value={info.loc}
-              onChange={handleChange}
-            />
-          </label>
+          <InputField
+            label="Start Date"
+            name="startdate"
+            value={info.startdate}
+            placeholder="e.g. September 2021"
+            onChange={handleChange}
+          />
+          <InputField
+            label="End Date"
+            name="enddate"
+            value={info.enddate}
+            placeholder="e.g. March 2024"
+            onChange={handleChange}
+          />
+          <InputField
+            label="Location"
+            name="loc"
+            value={info.loc}
+            placeholder="e.g. New Delhi, India"
+            onChange={handleChange}
+          />
 
           {!showAddWork ? (
             <NewButton
-              onClick={handleSubmit}
+              onClick={() => handleSubmit(noEntry ? "empty" : "add")}
               isFormValid={isFormValid}
               className="section-submit"
               buttonText="Add Work Experience"
             />
           ) : (
             <NewButton
-              onClick={editEntry}
+              onClick={() => updateWorkData(info)}
               isFormValid={isFormValid}
               className="section-edit"
               buttonText="Edit"
