29cac7e2dfaf058fc5df5896ae66e60471460fbe
Karl Vincent M. Espiritu <karlespiritukarl@gmail.com>
2024-08-16 04:41:28 +0800

    Made use of generic components - InputField and NewButton. Also put useEffect for proper state management

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,105 +1,82 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../styles/form.css";
+import NewButton from "./NewButton";
+import InputField from "./InputField";
 
 function GeneralInfo({ updateCV, cvData }) {
-  const [info, setInfo] = useState(cvData.generalInfo);
-  if (cvData.generalInfo !== info) {
-    setInfo(cvData.generalInfo);
-  }
-  const showGeneralInfo = info.name === "";
+  const [info, setInfo] = useState(cvData);
+
+  useEffect(() => {
+    if (cvData) {
+      setInfo(cvData);
+    }
+  }, [cvData]);
 
   function handleChange(e) {
     const { name, value } = e.target;
-    setInfo((prevInfo) => ({
-      ...prevInfo,
-      [name]: value,
-    }));
+    setInfo((prevInfo) => ({ ...prevInfo, [name]: value }));
   }
 
   function handleSubmit() {
     updateCV("generalInfo", info);
   }
 
-  return (
-    <>
-      <section className="general-info poppins-regular">
-        <label htmlFor="name">
-          Name
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={info.name}
-            placeholder="e.g. John Doe"
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="email">
-          Email
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={info.email}
-            placeholder="e.g. john.doe@example.com"
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="tel">
-          Cellphone #
-          <input
-            type="tel"
-            id="tel"
-            name="tel"
-            value={info.tel}
-            placeholder="e.g. +1234567890"
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="loc">
-          Address
-          <input
-            type="text"
-            id="loc"
-            name="loc"
-            value={info.loc}
-            placeholder="e.g. 118 Dr. Jose Rizal St., Manila"
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="linkedin">
-          LinkedIn
-          <input
-            type="text"
-            id="linkedin"
-            name="linkedin"
-            value={info.linkedin}
-            placeholder="e.g. linkedin.com/johndoe112"
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="github">
-          GitHub
-          <input
-            type="text"
-            id="github"
-            name="github"
-            value={info.github}
-            placeholder="e.g. github.com/johndoemas112"
-            onChange={handleChange}
-          />
-        </label>
+  const isFormValid = Object.values(info).every((value) => value.trim() !== "");
 
-        <button
-          type="button"
-          onClick={handleSubmit}
-          className="section-submit roboto-medium"
-          disabled={showGeneralInfo}
-        >
-          Edit
-        </button>
-      </section>
-    </>
+  return (
+    <section className="general-info poppins-regular">
+      <InputField
+        label="Name"
+        name="name"
+        value={info.name}
+        placeholder="e.g. John Doe"
+        onChange={handleChange}
+      />
+      <InputField
+        label="Email"
+        name="email"
+        value={info.email}
+        placeholder="e.g. john.doe@example.com"
+        type="email"
+        onChange={handleChange}
+      />
+      <InputField
+        label="Cellphone #"
+        name="tel"
+        value={info.tel}
+        placeholder="e.g. +1234567890"
+        type="tel"
+        onChange={handleChange}
+      />
+      <InputField
+        label="Address"
+        name="loc"
+        value={info.loc}
+        placeholder="e.g. 118 Dr. Jose Rizal St., Manila"
+        onChange={handleChange}
+      />
+      <InputField
+        label="LinkedIn"
+        name="linkedin"
+        value={info.linkedin}
+        placeholder="e.g. linkedin.com/johndoe112"
+        onChange={handleChange}
+      />
+      <InputField
+        label="GitHub"
+        name="github"
+        value={info.github}
+        placeholder="e.g. github.com/johndoemas112"
+        onChange={handleChange}
+      />
+
+      <NewButton
+        onClick={handleSubmit}
+        isFormValid={isFormValid}
+        className="section-submit"
+        buttonText="Edit"
+      />
+    </section>
   );
 }
 
